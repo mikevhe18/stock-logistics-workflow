694576109f4a32cf9dcad6d1480ef883aab4c946
mike <viriyananda.michael@gmail.com>
2016-09-08 12:56:18 +0700

    Adding module stock_account_ownership_availability_rules

---
+++ stock_account_ownership_availability_rules/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Account Ownership Availability Rules',
+    'version': '8.0.1.0.0',
+    'summary': '-',
+    'author': 'OpenSynergy Indonesia,Odoo Community Association (OCA)',
+    'website': 'https://opensynergy-indonesia.com',
+    'category': 'Stock',
+    'depends': [
+        'stock_account',
+        'stock_ownership_availability_rules'
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+}
